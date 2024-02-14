d004743a23d8f194cb18a4055548f0bf3984f58e
sparky-sg <sg.saksham2005@gmail.com>
2024-02-14 12:12:15 +0530

    jwt auth was added

--- server/Cookie-auth/index.js
+++ server/Cookie-auth/index.js
@@ -19,21 +19,45 @@ const authenticateUser = (req, res, next) => {
   next();
 };
 
+// app.post('/login', (req, res) => {
+//   const { username, password } = req.body;
+//   if (!username || !password) {
+//     return res.status(400).json({ error: 'Username and password are required' });
+//   }
+
+//   const user = users.find(user => user.username === username && user.password === password);
+//   if (!user) {
+//     return res.status(401).json({ error: 'Invalid username or password' });
+//   }
+
+//   res.cookie('username', username);
+//   console.log('Username cookie set:', username);
+//   return res.json({ message: 'Login successful', username });
+// });
+
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ error: 'Username and password are required' });
-  }
+  const { email, password } = req.body;
 
-  const user = users.find(user => user.username === username && user.password === password);
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid username or password' });
-  }
-
-  res.cookie('username', username);
-  console.log('Username cookie set:', username);
-  return res.json({ message: 'Login successful', username });
+  const token = jwt.sign({ email }, JWT_SECRET_KEY, { expiresIn: '1h' });
+  res.cookie('token', token, { httpOnly: true });
+  res.json({ success: true });
 });
+
+function authenticateToken(req, res, next) {
+const token = req.cookies.token;
+
+if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: Missing token' });
+}
+
+jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+        return res.status(403).json({ error: 'Forbidden: Invalid token' });
+    }
+    req.user = decoded;
+    next();
+})}
+
 
 app.get('/logout', (req, res) => {
   res.clearCookie('username');
